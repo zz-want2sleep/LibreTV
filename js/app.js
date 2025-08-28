a6bca3afbc86b2692480d97675206a3bf83af205
zz-want2sleep <49852350+zz-want2sleep@users.noreply.github.com>
2025-08-28 09:02:08 +0800

    Update app.js
    默认选中资源

--- js/app.js
+++ js/app.js
@@ -28,7 +28,7 @@ document.addEventListener('DOMContentLoaded', function () {
     // 设置默认API选择（如果是第一次加载）
     if (!localStorage.getItem('hasInitializedDefaults')) {
         // 默认选中资源
-        selectedAPIs = ["tyyszy", "bfzy", "dyttzy", "ruyi"];
+        selectedAPIs = ["taohua", "niuniu"];
         localStorage.setItem('selectedAPIs', JSON.stringify(selectedAPIs));
 
         // 默认选中过滤开关
